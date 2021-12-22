9807f9fad35287caa19dcad2c379f2bd077b8551
Hermann Vincent <baw.developpement@gmail.com>
2021-12-22 21:47:56 +0400

    better converter dat obj

--- tools/rotmg_xmls_to_json/index.js
+++ tools/rotmg_xmls_to_json/index.js
@@ -46,5 +46,83 @@ function xmlToJsonFile () {
 }
 
 function newData () {
-    
+    fs.readFile(__dirname + fileToParse + ".json", { encoding : "utf-8" }, function(err, data) {
+        let parsed = JSON.parse(data).Object
+        let classes = {}
+        Object.keys(parsed).forEach((index) => {
+            if (Object.keys(classes).findIndex((v) => v == parsed[index].Class) == -1 && typeof(parsed[index].Class) !== "undefined") {
+                let newObj = {} 
+                Object.keys(parsed[index]).forEach(objKey => {
+                    if (objKey === "$") {
+                        Object.keys(parsed[index][objKey]).forEach(o => {
+                            newObj[o] = parsed[index][objKey][o]
+                        })
+                    }
+                    else {
+                        if ( parsed[index][objKey] == "") { newObj[objKey] = true }
+                        else if (typeof(parsed[index][objKey]) == "object" ) {
+                            if ( parsed[index][objKey]["_"] !== null && parsed[index][objKey]["_"] !== undefined) {
+                                if (parsed[index][objKey]["_"] == "0" || parsed[index][objKey]["_"] == "100") {
+                                    newObj[objKey] = {
+                                        default : parsed[index][objKey]["_"],
+                                        max : parsed[index][objKey]["$"]
+                                    }
+                                } else {
+                                    newObj[objKey] = {}
+                                    newObj[objKey][
+                                        parsed[index][objKey]["_"]
+                                    ] = parsed[index][objKey]["$"]
+                                }
+                            }
+                        }
+                        else {
+                            newObj[objKey] = parsed[index][objKey]
+                        }
+                    }
+                })
+                classes[parsed[index].Class] = [newObj]
+            } else if (Object.keys(classes).findIndex((v) => v == parsed[index].Class) !== -1) { 
+                let newObj = {} 
+                Object.keys(parsed[index]).forEach(objKey => {
+                    if (objKey === "$") {
+                        Object.keys(parsed[index][objKey]).forEach(o => {
+                            newObj[o] = parsed[index][objKey][o]
+                        })
+                    }
+                    else {
+                        if ( parsed[index][objKey] == "") { newObj[objKey] = true }
+                        else if (typeof(parsed[index][objKey]) == "object" ) {
+                            if ( parsed[index][objKey]["_"] !== null && parsed[index][objKey]["_"] !== undefined) {
+                                if (parsed[index][objKey]["_"] == "0") {
+                                    newObj[objKey] = {
+                                        default : parsed[index][objKey]["_"],
+                                        max : parsed[index][objKey]["$"]
+                                    }
+                                } else {
+                                    newObj[objKey] = {}
+                                    newObj[objKey][parsed[index][objKey]["_"]] = parsed[index][objKey]["$"]
+                                }
+                            }
+                        }
+                        else {
+                            newObj[objKey] = parsed[index][objKey]
+                        }
+                    }
+                })
+                classes[parsed[index].Class].push(newObj)
+            }
+        })
+
+        Object.keys(classes).forEach((cls) => {
+            fs.writeFile(__dirname + "/bin/" + cls + ".json", JSON.stringify(classes[cls], null, 2), { encoding: "utf-8" }, (err) => {
+                if (!err) {
+                    console.log("success")
+                } else {
+                    console.err(err)
+                }
+            })
+        })
+    });
 }
+
+newData()
