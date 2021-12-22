05539162c8eb33a939cd89b12fea5a68d77a590a
Hermann Vincent <baw.developpement@gmail.com>
2021-12-22 20:14:14 +0400

    set all ui element relative to screen size

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -106,17 +106,15 @@ local bagInterface = {
 
 function Player:drawPlus ( index )
     local w, h = love.window:getMode()
-    local realCamX = 0
-    local realCamY = 0
-    love.graphics.rectangle("line", realCamX + plus[index].x - 8, realCamY + plus[index].y - 8, 16, 16)
+    love.graphics.rectangle("line", plus[index].x - 8, h - (600 - plus[index].y)- 8, 16, 16)
 
     local character = self.components["CharacterComponent"]
     if character.statPoints > 0 then
-        if mx > plus[index].x - 8 and mx < plus[index].x + 8 and my > plus[index].y - 8 and my < plus[index].y + 8 then
+        if mx > plus[index].x - 8 and mx < plus[index].x + 8 and my >  h - (600 - plus[index].y) - 8 and my < h - (600 - plus[index].y) + 8 then
             love.graphics.setColor(255,255,0,255)
         end
-        love.graphics.rectangle("fill", realCamX +  plus[index].x - 2, realCamY + plus[index].y - 8, 4, 16)
-        love.graphics.rectangle("fill", realCamX +  plus[index].x - 8, realCamY + plus[index].y - 2, 16, 4)
+        love.graphics.rectangle("fill",  plus[index].x - 2,  h - (600 - plus[index].y) - 8, 4, 16)
+        love.graphics.rectangle("fill",  plus[index].x - 8,  h - (600 - plus[index].y) - 2, 16, 4)
         love.graphics.setColor(255,255,255,255)
     end
 end
@@ -132,14 +130,15 @@ local quickSlots = {
 }
 --#endregion
 local function drawQuickSlots ()
+    local w, h = love.window.getMode()
     for i, v in ipairs(quickSlots) do
         love.graphics.setColor(1,1,1,1)
-        love.graphics.rectangle("line", 250 + (i - 1) * 42, 600 - 110, 32, 32)
+        love.graphics.rectangle("line", w / 2 - 150 + (i - 1) * 42, h - 110, 32, 32)
         love.graphics.setColor(0,0,0,0.4)
-        love.graphics.rectangle("fill", 250 + (i - 1) * 42, 600 - 110, 32, 32)
+        love.graphics.rectangle("fill", w / 2 - 150 + (i - 1) * 42, h - 110, 32, 32)
         love.graphics.setColor(1,1,1,1)
         if quickSlots[i].item ~= nil and quickSlots[i].quantity ~= 0 and quickSlots[i].quantity ~= nil then
-            love.graphics.print(""..quickSlots[i].quantity, 250 + (i - 1) * 42, 600 - 110)
+            love.graphics.print(""..quickSlots[i].quantity, w / 2 - 150 + (i - 1) * 42, h - 110)
             local quad = love.graphics.newQuad(
                 0,
                 0,
@@ -149,8 +148,8 @@ local function drawQuickSlots ()
             love.graphics.draw(
                 quickSlots[i].item.texture,
                 quad,
-                250 + (i - 1) * 42,
-                600 - 110,
+                w / 2 + (i - 1) * 42,
+                h - 110,
                 0,
                 1.5, 1.5,
                 0, 0,
@@ -176,69 +175,68 @@ function Player:draw()
     local selfPosition = self.components["TransformComponent"].position
     local stats = characterComponent.stats
 
-    local ww, wh = love.window.getMode()
     self.healthText:set(""..characterComponent.stats.life .. " / " .. characterComponent.stats.max_life)
     self.manaText:set(""..characterComponent.stats.mana .. "\n / \n" .. characterComponent.stats.max_mana)
     self.expText:set(""..characterComponent.exp .. " / " .. characterComponent.max_exp)
 
     love.graphics.setColor(255, 0, 0, 255);
-    love.graphics.rectangle("fill", realCamX + 800 / 2 - 150, realCamY + 600 - 68, 300 * ( stats.life / stats.max_life ), 32)
+    love.graphics.rectangle("fill", w / 2 - 150, h - 68, 300 * ( stats.life / stats.max_life ), 32)
     love.graphics.setColor(255, 255, 255, 255);
-    love.graphics.rectangle("line", realCamX + 800 / 2 - 150, realCamY + 600 - 68, 300, 32)
-    love.graphics.draw(self.healthText,  ww / 2 - self.healthText:getWidth() / 2, 532 + (self.healthText:getHeight() / 2))
+    love.graphics.rectangle("line", w / 2 - 150, h - 68, 300, 32)
+    love.graphics.draw(self.healthText,  w / 2 - self.healthText:getWidth() / 2, h - 68 + (self.healthText:getHeight() / 2))
     -- Mana
     love.graphics.setColor(0,255,200,255);
-    love.graphics.rectangle("fill", realCamX + 564, realCamY + 600 - 74, 64, 64)
+    love.graphics.rectangle("fill", w / 2 + 164, h - 74, 64, 64)
     love.graphics.setColor(255, 255, 255, 255);
-    love.graphics.rectangle("line", realCamX + 564, realCamY + 600 - 74, 64, 64 * ( stats.mana / 100 ))
-    love.graphics.draw(self.manaText,  564 + 32 - self.manaText:getWidth(), 526 + (self.manaText:getHeight() / 2))
+    love.graphics.rectangle("line", w / 2 + 164, h - 74, 64, 64 * ( stats.mana / 100 ))
+    love.graphics.draw(self.manaText,  w / 2 + 164 + 16 + 32 - self.manaText:getWidth(),h - 74 + (self.manaText:getHeight() / 2))
     -- Experience
     love.graphics.setColor(0, 0, 0, 0.4);
-    love.graphics.rectangle("fill", realCamX + 800 / 2 - 150, realCamY + 600 - 26, 300, 16)
+    love.graphics.rectangle("fill", w / 2 - 150, h - 26, 300, 16)
     love.graphics.setColor(128/255, 0, 128/255, 1);
-    love.graphics.rectangle("fill", realCamX + 800 / 2 - 150, realCamY + 600 - 26, 300 * ( characterComponent.exp / characterComponent.max_exp ), 16)
+    love.graphics.rectangle("fill", w / 2 - 150, h - 26, 300 * ( characterComponent.exp / characterComponent.max_exp ), 16)
     love.graphics.setColor(255, 255, 255, 255);
-    love.graphics.rectangle("line", realCamX + 800 / 2 - 150, realCamY + 600 - 26, 300, 16)
-    love.graphics.draw(self.expText,  ww / 2 - self.expText:getWidth() / 2, 576)
+    love.graphics.rectangle("line", w / 2 - 150, h - 26, 300, 16)
+    love.graphics.draw(self.expText,  w / 2 - self.expText:getWidth() / 2, h - 24)
 
     -- PlAYER
     if charInterface.show then
         -- Button
         love.graphics.setColor(128 / 255, 0, 128 / 255, 255);
-        love.graphics.rectangle("fill", realCamX + 10, realCamY + 600 - 32 - 10, 32, 32)
+        love.graphics.rectangle("fill", 10, h - 32 - 10, 32, 32)
         love.graphics.setColor(1, 1, 1, 1);
-        love.graphics.print("C", realCamX +16, realCamY + 600-32)
-        love.graphics.rectangle("line", realCamX + 10, realCamY + 600 - 32 - 10, 32, 32)
+        love.graphics.print("C", 16, h - 32)
+        love.graphics.rectangle("line", 10, h - 32 - 10, 32, 32)
 
         -- INTERFACE
         love.graphics.setColor(1, 1, 1, 1);
-        love.graphics.rectangle("line", realCamX + 10, realCamY + 600 - 272, 220, 220);
+        love.graphics.rectangle("line", 10, h - 272, 220, 220);
         love.graphics.setColor(0, 0, 0, 0.4);
-        love.graphics.rectangle("fill", realCamX + 10, realCamY + 600 - 272, 220, 220);
+        love.graphics.rectangle("fill", 10, h - 272, 220, 220);
         love.graphics.setColor(1, 1, 1, 1);
-        love.graphics.print("Player stats", realCamX + 10, realCamY + 308)
-        love.graphics.print("Max life : "..stats.max_life, realCamX + 40, realCamY + 338)
+        love.graphics.print("Player stats", 10, 308)
+        love.graphics.print("Max life : "..stats.max_life, 40, h - 262) 
         self:drawPlus(1)
-        love.graphics.print("Max mana : "..stats.max_mana, realCamX + 40, realCamY + 358)
+        love.graphics.print("Max mana : "..stats.max_mana, 40, h - 242)
         self:drawPlus(2)
-        love.graphics.print("Attack : "..stats.attack, realCamX + 40, realCamY + 378)
+        love.graphics.print("Attack : "..stats.attack, 40, h - 222)
         self:drawPlus(3)
-        love.graphics.print("Defense : "..stats.defense, realCamX + 40, realCamY + 398)
+        love.graphics.print("Defense : "..stats.defense, 40, h - 202)
         self:drawPlus(4)
-        love.graphics.print("Wisdom : "..stats.wisdom, realCamX + 40, realCamY + 418)
+        love.graphics.print("Wisdom : "..stats.wisdom, 40, h - 182)
         self:drawPlus(5)
-        love.graphics.print("Dexterity : "..stats.dexterity, realCamX + 40, realCamY + 438)
+        love.graphics.print("Dexterity : "..stats.dexterity, 40, h - 162)
         self:drawPlus(6)
-        love.graphics.print("Speed : "..stats.speed, realCamX + 40, realCamY + 458)
+        love.graphics.print("Speed : "..stats.speed, 40, h - 142)
         self:drawPlus(7)
-        love.graphics.print("Force : "..stats.force, realCamX + 40, realCamY + 478)
+        love.graphics.print("Force : "..stats.force, 40, h - 122)
         self:drawPlus(8)
     else
         love.graphics.setColor(0, 0, 0, 0.4);
-        love.graphics.rectangle("fill", realCamX + 10, realCamY + 600 - 32 - 10, 32, 32)
+        love.graphics.rectangle("fill", 10, 600 - 32 - 10, 32, 32)
         love.graphics.setColor(1, 1, 1, 1);
-        love.graphics.print("C", realCamX +16, realCamY + 600-32)
-        love.graphics.rectangle("line", realCamX + 10, realCamY + 600 - 32 - 10, 32, 32)
+        love.graphics.print("C", 16, h-32)
+        love.graphics.rectangle("line", 10, h - 32 - 10, 32, 32)
     end
 
     -- BAG
@@ -246,16 +244,16 @@ function Player:draw()
         -- Bag Icon
         self.bag:draw()
         love.graphics.setColor(128/255, 0, 128/255, 255);
-        love.graphics.rectangle("fill", 20 + 32, 600 - 32 - 10, 32, 32)
+        love.graphics.rectangle("fill", 20 + 32, h - 32 - 10, 32, 32)
         love.graphics.setColor(1, 1, 1, 1);
-        love.graphics.print("B", 65, 600-32)
-        love.graphics.rectangle("line", 20 + 32, 600 - 32 - 10, 32, 32)
+        love.graphics.print("B", 65, h-32)
+        love.graphics.rectangle("line", 20 + 32, h - 32 - 10, 32, 32)
     else
         love.graphics.setColor(0, 0, 0, 0.4);
-        love.graphics.rectangle("fill", 20 + 32, 600 - 32 - 10, 32, 32)
+        love.graphics.rectangle("fill", 20 + 32, h - 32 - 10, 32, 32)
         love.graphics.setColor(1, 1, 1, 1);
-        love.graphics.print("B", 65, 600-32)
-        love.graphics.rectangle("line", 20 + 32, 600 - 32 - 10, 32, 32)
+        love.graphics.print("B", 65, h-32)
+        love.graphics.rectangle("line", 20 + 32, h - 32 - 10, 32, 32)
     end
 end
 
@@ -263,10 +261,11 @@ local lshift = false
 function Player:mousepressed(mousex, mousey, button)
     Entity.mousepressed(self, mousex, mousey, button)
     self.bag:mousepressed(mousex, mousey, button)
+    local w, h = love.window.getMode()
     local characterComponent = self.components["CharacterComponent"]
     if characterComponent.statPoints > 0 then
         for i, v in ipairs(plus) do
-            if mousex > plus[i].x - 8 and mousex < plus[i].x + 8 and mousey > plus[i].y - 8 and mousey < plus[i].y + 8 then
+            if mousex > plus[i].x - 8 and mousex < plus[i].x + 8 and mousey > h - (600 - plus[i].y) - 8 and mousey < h - (600 - plus[i].y) + 8 then
                 if button == 1 then
                     if lshift ~= false then
                         if plus[i].name == "max_life" or plus[i].name == "max_mana" then
