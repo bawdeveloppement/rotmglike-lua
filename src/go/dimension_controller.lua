3373525fec764e8f49f2f1d5cc269c5032c374e9
Hermann Vincent <baw.developpement@gmail.com>
2021-12-23 13:40:19 +0400

    handle default element in DimensionControllerObject

--- src/go/dimension_controller.lua
+++ src/go/dimension_controller.lua
@@ -2,10 +2,17 @@ local DimensionController = require(_G.libDir .. "middleclass")("DimensionContro
 local SelectElement = require(_G.engineDir .. "game_objects.main").InterfaceElements.SelectElement
 
 function DimensionController:initialize(x, y)
-
+    local w, h = love.window.getMode()
+    local alreadyI = "default"
+    for i, v in ipairs(_G.xle.Init.optionsCached) do
+        if v.value.width == w and v.value.height == h then
+            alreadyI = v.id
+        end
+    end
     self.select = SelectElement:new(
         { x = x, y = y, width = 200, height = 32 },
-        _G.xle.Init.optionsCached
+        _G.xle.Init.optionsCached,
+        alreadyI
     )
 end
 
