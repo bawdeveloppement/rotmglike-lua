5d926d1f148b01f89ba179d7d2b34cfe43ce5a0c
Hermann Vincent <baw.developpement@gmail.com>
2022-06-17 07:12:41 +0200

    remove camera system

--- src/worlds/realm/realm.lua
+++ src/worlds/realm/realm.lua
@@ -5,13 +5,11 @@ local RealmData = require(_G.srcDir .. "worlds.realm.data")
 
 -- Systems
 local WorldBossSystem = require(_G.srcDir .. "systems.system-world_boss")
-local CameraSystem = require(_G.srcDir .. "systems.system-camera")
 
 function Realm:initialize (screen, active)
   World.initialize(self, screen, active or false, "realm", RealmData, 4);
 
   self:addSystem(WorldBossSystem:new(self))
-  self:addSystem(CameraSystem:new(self))
 end
 
 return Realm
